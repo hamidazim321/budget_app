bed5d5863c6f7d797efe4a0b6951c82024b66e42
hamid321 <hamidazim321@gmail.com>
2024-02-05 12:45:23 +0500

    Fix linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   has_many :group_transactions
   has_many budget_transactions, through: :group_transactions
 
--- test/models/budget_transaction_test.rb
+++ test/models/budget_transaction_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BudgetTransactionTest < ActiveSupport::TestCase
   # test "the truth" do
