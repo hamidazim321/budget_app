2a3fb88b4c687a540819adc0f1647492e9a69fe9
hamid321 <hamidazim321@gmail.com>
2024-02-06 13:32:48 +0500

    Add create action for categories

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,25 @@
 class GroupsController < ApplicationController
-  def index; end
+  def index
+    @groups = Group.where(author_id: current_user.id)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = 'Category created Successfully'
+      redirect_to groups_path
+    else
+      flash[:error] = @group.errors.full_messages.to_sentence
+      redirect_to new_group_path
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :icon).merge(author: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "groups#index"
+  resources :groups
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
