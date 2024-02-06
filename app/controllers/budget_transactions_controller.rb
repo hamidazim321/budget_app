c859033407a19f2872da761e09577079bb0d51b6
hamid321 <hamidazim321@gmail.com>
2024-02-06 17:33:28 +0500

    Add budget_transactions#show

--- app/controllers/budget_transactions_controller.rb
+++ app/controllers/budget_transactions_controller.rb
@@ -1,9 +1,14 @@
 class BudgetTransactionsController < ApplicationController
 
+  def show
+    @budget_transaction = BudgetTransaction.find_by_id(params[:id])
+    @group = Group.find_by_id(params[:group_id])
+  end
+
   def new
     @budget_transaction = BudgetTransaction.new
   end
-  
+
   def create
     @budget_transaction = current_user.budget_transactions.build(budget_transaction_params)
 
