70724360f44e1f6c9e16309c37cc1abad0337d3f
hamid321 <hamidazim321@gmail.com>
2024-02-07 16:24:56 +0500

    fix linter errors

--- spec/features/budget_transaction_spec.rb
+++ spec/features/budget_transaction_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "BudgetTransactions", type: :feature do
+RSpec.feature 'BudgetTransactions', type: :feature do
   before(:each) do
     @user = User.create(name: 'hamid', email: 'hdm@gmail.com', password: '123456', password_confirmation: '123456',
                         id: 1)
@@ -11,25 +11,27 @@ RSpec.feature "BudgetTransactions", type: :feature do
     visit group_budget_transaction_path(@group, @budget_transaction)
   end
 
-  context 'Get /Show' do 
-    it 'header should contain the text "Transaction"' do 
-      within('header') do 
+  context 'Get /Show' do
+    it 'header should contain the text "Transaction"' do
+      within('header') do
         expect(page).to have_content('Transaction')
       end
     end
 
-    it 'should show details about the transaction' do 
+    # rubocop:disable Layout/LineLength
+    it 'should show details about the transaction' do
       expect(page).to have_content(@budget_transaction.name)
       expect(page).to have_content(@budget_transaction.amount)
-      expect(page).to have_content("#{@budget_transaction.created_at.strftime("%d %B %Y")} At #{@budget_transaction.created_at.in_time_zone.strftime("%I:%M %p")}")
+      expect(page).to have_content("#{@budget_transaction.created_at.strftime('%d %B %Y')} At #{@budget_transaction.created_at.in_time_zone.strftime('%I:%M %p')}")
     end
+    # rubocop:enable Layout/LineLength
 
-    it 'should have a button to delete the transaction' do 
+    it 'should have a button to delete the transaction' do
       expect(page).to have_button('Delete Transaction')
     end
 
-    it 'clicking delete transaction button should delete the transaction' do 
-      budget_transactions_count = BudgetTransaction.count 
+    it 'clicking delete transaction button should delete the transaction' do
+      budget_transactions_count = BudgetTransaction.count
       click_button('Delete Transaction')
       expect(BudgetTransaction.count).to eq(budget_transactions_count - 1)
     end
