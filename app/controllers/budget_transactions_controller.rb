e04d74e58cd2b3f4e9a02fb4dc8563da9e89885b
hamid321 <hamidazim321@gmail.com>
2024-02-07 14:39:03 +0500

    Fix linter errors

--- app/controllers/budget_transactions_controller.rb
+++ app/controllers/budget_transactions_controller.rb
@@ -18,9 +18,9 @@ class BudgetTransactionsController < ApplicationController
     @budget_transaction = current_user.budget_transactions.build(budget_transaction_params)
     if @budget_transaction.save
       GroupTransaction.create(budget_transaction: @budget_transaction, group_id: @group)
-      unless @groups == nil || @groups.empty?
+      unless @groups.nil? || @groups.empty?
         @groups.each do |group_id|
-          GroupTransaction.create(budget_transaction: @budget_transaction, group_id: group_id)
+          GroupTransaction.create(budget_transaction: @budget_transaction, group_id:)
         end
       end
       redirect_to group_path(@group)
@@ -35,7 +35,7 @@ class BudgetTransactionsController < ApplicationController
     @budget_transaction = BudgetTransaction.find_by_id(params[:id])
     group_id = params[:group_id]
     if @budget_transaction.destroy
-      redirect_to group_path(group_id), notice: "Transaction deleted"
+      redirect_to group_path(group_id), notice: 'Transaction deleted'
     else
       flash[:error] = @budget_transaction.errors.full_messages.to_sentence
     end
@@ -46,5 +46,4 @@ class BudgetTransactionsController < ApplicationController
   def budget_transaction_params
     params.require(:budget_transaction).permit(:name, :amount)
   end
-
 end
