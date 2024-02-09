3ef8dbb79d535fa954481a2109233641de5345e9
hamid321 <hamidazim321@gmail.com>
2024-02-09 14:53:18 +0500

    Fix linter errors

--- spec/features/group_show_spec.rb
+++ spec/features/group_show_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.feature 'Groups', type: :feature do
   before(:each) do
     @user = User.create(name: 'hamid', email: 'hdm@gmail.com', password: '123456', password_confirmation: '123456')
@@ -7,34 +5,9 @@ RSpec.feature 'Groups', type: :feature do
     @group = Group.create(author_id: @user.id, name: 'test', icon: 'http://test.com')
     @group_transaction = GroupTransaction.create(budget_transaction: @budget_transaction, group: @group)
     sign_in @user
+    visit group_path(@group)
   end
-
-  describe 'Get /Index' do
-    before(:each) do
-      visit groups_path
-    end
-    it 'should display category details' do
-      expect(page).to have_content(@group.name)
-      expect(page).to have_content("Total Spendings $#{@budget_transaction.amount}")
-      expect(page).to have_content(@group.created_at.strftime('%d %B %Y'))
-    end
-
-    it 'should display the text "categories in the header"' do
-      within('nav') do
-        expect(page).to have_content('Categories')
-      end
-    end
-
-    it 'should have a link to add new category' do
-      expect(page).to have_link('Add new category')
-    end
-  end
-
-  describe 'Get /Show' do
-    before(:each) do
-      visit group_path(@group)
-    end
-
+  context 'Group/show' do
     it 'should display details about budget transactions in the category' do
       expect(page).to have_content(@budget_transaction.name)
       expect(page).to have_content(@group_transaction.created_at.strftime('%d %B %Y'))
